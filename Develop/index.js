8e3e49f8d251299650f67cac03e50f64f571a87c
Ahmad Bustami <ahmadbustami@Ahmads-MacBook-Pro.local>
2023-02-15 22:27:36 -0600

    working hard

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown= require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [];
+// Table of Contents, 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'Title',
+      message: 'What is your Title?',
+    },
+    {
+      type: 'input',
+      name: 'Description',
+      message: 'a description?',
+    },
+    {
+      type: 'input',
+      name: 'Installation',
+      message: 'Installation process?',
+    },
+    // {
+    //   type: 'input',
+    //   name: 'usage',
+    //   message: 'What is it used for?',
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'licenses',
+    //   message: 'what license do you have?',
+    //   choices:'',
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'contributing',
+    //   message: 'any contributions',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'tests',
+    //     message: 'what did you test?',
+        
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'questions',
+    //     message: 'any questions',
+    //   },
+  ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  .then((response) => {
+    fs.writeFile('README.md', generateMarkdown(response), (err) =>
+      err ? console.log(err) : console.log('Successfully created readme!')
+    )
+  });
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
